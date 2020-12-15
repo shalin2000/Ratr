5b6018e29bd5924ef68b052cf56f450b8a08855b
Qasim Mir <mir@Qasims-MacBook-Pro.local>
2020-12-15 12:48:23 -0600

    Starter screen

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from "react-native";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo, hello</Text>
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <Text style = {styles.title}> 
+      Ratr
+    </Text>
+    <Text style = {styles.secondary} >
+      Rate. Track. Enjoy.
+    </Text>
+    <View>
+      <Button
+        title="Login"
+        onPress={() => Alert.alert('Button pressed')}
+      />
     </View>
-  );
-}
+    <Separator />
+    <View>
+      <Button
+        title="Continue as Guest"
+        color="#f194ff"
+        onPress={() => Alert.alert('Color button pressed')}
+      />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 50, 
+    fontWeight: 'bold', 
+    fontStyle: 'italic', 
+    color: 'black',
+    textAlign: 'center',
+  },
+  secondary: {
+    textAlign: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: 'white',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;

9c280440aecde1716416d7e109724eb49dd6cc69
Shalin Patel <57925316+shalin2000@users.noreply.github.com>
2020-12-17 16:04:12 -0600

    stupid connecting issues between googleAPI and NYTIMES API

--- Components/BookScreen.js
+++ Components/BookScreen.js
@@ -5,34 +5,66 @@ class BookScreen extends React.Component {
 	constructor(props){
 			super(props);
 			this.state = {
-				googleBook: [],
-				NYBook: []
+				volumeInfo: [],
+				saleInfo: [],
+				NYBook: [],
 			};
 	}
 
 	componentDidMount(){
-		fetch('https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyC2uH3lMt5kv43Ys9p34UGWPtJymgOc-Qk', {
-          method: 'GET'
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-        this.setState({googleBook: responseJson})
-      })
-      .catch((error) => {
-          console.error(error);
-		});
-		
-
 		this.setState({
 			NYBook: this.props.route.params.book
 		});
-		console.log(this.props.route.params.book)
+		this.fetchAPI(this.props.route.params.book.title)
+	}
+
+	fetchAPI(bookTitle){
+		var title = bookTitle.split(' ').join('_');
+		fetch('https://www.googleapis.com/books/v1/volumes?q='+title+'&key=AIzaSyC2uH3lMt5kv43Ys9p34UGWPtJymgOc-Qk', {
+				method: 'GET'
+		})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			console.log(responseJson)
+			var x;
+			for (x of responseJson.items){
+				if (((x.volumeInfo.industryIdentifiers[0].identifier === this.props.route.params.book.isbns[0].isbn13) ||
+						(x.volumeInfo.industryIdentifiers[0].identifier === this.props.route.params.book.isbns[1].isbn13) ||
+						(x.volumeInfo.industryIdentifiers[1].identifier === this.props.route.params.book.isbns[0].isbn10) ||
+						(x.volumeInfo.industryIdentifiers[1].identifier === this.props.route.params.book.isbns[1].isbn10)) ||
+						((x.volumeInfo.title === this.props.route.params.book.title) && (x.volumeInfo.authors === this.props.route.params.book.author))){
+					this.storeBook(x)
+				}
+			}
+			this.storeAllBooks(responseJson)
+		})
+		.catch((error) => {
+				console.error(error);
+		});
+	}
+
+	// Stores the correct book that was selected from the NYTIMES
+	storeBook(book){
+		this.setState({
+			volumeInfo: book.volumeInfo,
+			saleInfo: book.saleInfo,
+		})
+	}
+
+	// Stores all results found from google api when user searched a specifc title
+	storeAllBooks(books){
+		
 	}
 
 	render() {
 		return (
 			<ScrollView style={styles.container}>
+
+				<Text style={styles.text}>{this.state.volumeInfo.title}</Text>
+				<Text style={styles.text}>{this.state.saleInfo.buyLink}</Text>
+
+
+
 				<Text style={styles.text}>{this.state.NYBook.title}</Text>
 				<Image source = {{uri:this.state.NYBook.book_image}} style = {styles.image} /> 
 			</ScrollView>
